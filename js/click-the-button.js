126d4c8a9d6a4a34fbc69b77e5c4205c70355c5f
Liam <liam.yy.xu@gmail.com>
2022-12-15 15:12:09 -0500

    Basically nothing

--- js/click-the-button.js
+++ js/click-the-button.js
@@ -1,5 +1,9 @@
 function start() {
     var gameButton = document.getElementById("game-button")
 
-    
+    var i = Math.floor(Math.random() * 500) + 1;
+    var j = Math.floor(Math.random() * 500) + 1;
+
+    gameButton.style.left = i+"px";
+    gameButton.style.top = j+"px";
 }
