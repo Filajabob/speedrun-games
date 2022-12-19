dd607ec64e22e5a7563a1d0ddbd49db1e7b44b6d
Liam <liam.yy.xu@gmail.com>
2022-12-19 10:09:01 -0500

    Finished Click the Button barebones

--- js/click-the-button.js
+++ js/click-the-button.js
@@ -1,9 +1,59 @@
-function start() {
-    var gameButton = document.getElementById("game-button")
+var timerWrapper = document.getElementById("timer")
+var timer = new Stopwatch(timerWrapper)
+
+var startButton = document.getElementById("start-button");
+var gameButton = document.getElementById("game-button");
+
+function sleep(s) {
+    return new Promise(resolve => setTimeout(resolve, s * 1000));
+}
+
+function timerUpdate() {
+    var timerDisplay = document.getElementById("timer")
+    timerDisplay.textContent = 0.0
+
+    var t = 0
+
+    timerDisplay.textContent = t + 0.01
+}
+
+function stop() {
+    timer.stop();
+
+    gameButton.disabled = true;
+    startButton.disabled = false;
+}
+
+function processButtonClick() {
+    var gameButton = document.getElementById("game-button");
+    var clickDisplay = document.getElementById("click-number");
+    var prevClicks = parseInt(clickDisplay.textContent)
+
+    clickDisplay.textContent = prevClicks + 1
+
+    if (parseInt(clickDisplay.textContent) >= 20) {
+        stop();
+    }
 
     var i = Math.floor(Math.random() * 500) + 1;
     var j = Math.floor(Math.random() * 500) + 1;
 
     gameButton.style.left = i+"px";
     gameButton.style.top = j+"px";
+}
+
+function start() {
+    gameButton.disabled = false;
+    startButton.disabled = true;
+
+    var i = Math.floor(Math.random() * 500) + 1;
+    var j = Math.floor(Math.random() * 500) + 1;
+
+    gameButton.style.left = i+"px";
+    gameButton.style.top = j+"px";
+
+    gameButton.addEventListener("click", processButtonClick)
+
+    
+    timer.start()
 }
